2497d8beb1198b36c455be25484646959a221be6
Ahmed Silat <ahmedsilat44@gmail.com>
2024-07-11 04:42:37 +0500

    Company Update

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 var btn = $('#backToTopButton');
+var navbar = $('#header');
 
-
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+    navbar.addClass('translucent');
+  } else {
+    btn.removeClass('show');
+    navbar.removeClass('translucent');
+  }
+});
 $(window).scroll(function() {
   if ($(window).scrollTop() > 300) {
     btn.addClass('show');
@@ -19,3 +28,37 @@ function scrollFunction(elementId) {
     element.scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" });
   }
 
+
+const hiddenImg = document.querySelectorAll('.hidden_img');
+
+const hiddenText = document.querySelectorAll('.hidden_text');
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible_img');
+    }
+    else {
+      entry.target.classList.remove('visible_img');
+    }
+  });
+});
+
+const observerTxt = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible_img');
+    }
+    else {
+      entry.target.classList.remove('visible_img');
+    }
+  });
+});
+
+hiddenImg.forEach((element) => {
+  observer.observe(element);
+});
+
+hiddenText.forEach((element) => {
+  observerTxt.observe(element);
+});
